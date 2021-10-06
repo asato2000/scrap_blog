25c9fc6a2bc119f4c0a774bce75c467911644c70
asato2000 <68488182+asato2000@users.noreply.github.com>
2021-10-06 17:05:43 +0900

    すべてをいっきにとれない。こうせいがちがう

--- example.rb
+++ example.rb
@@ -22,13 +22,13 @@ html = File.open('academy.html', 'r') {|f| f.read }
 doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
 
 tech = []
-doc.xpath('//*[@id="contents"]/div[1]/div[2]/div/div[2]/div[2]').each do |section|
+doc.xpath('//*[@id="contents"]/div[1]/div[2]/div/div[2]/div[1]').each do |section|
 
  contents = {category: nil, topic: []}
  contents[:category] = section.xpath('./h2').first.text
- section.xpath('./div/div').each do |node|
-  title = node.xpath('./div/h3/a').first.text
-  url = node.xpath('./div/h3/a').first['href']
+ section.xpath('./div').each do |node|
+  title = node.xpath('./div/h4/a').first.text
+  url = node.xpath('./div/h4/a').first["href"]
  topic = {title: title, url: url}
  contents[:topic] << topic 
  
@@ -42,7 +42,9 @@ pp tech
 # そもそも
 # /html/body/div[2]/div[1]/div[2]/div/div[2]/div[2]
 # /html/body/div[2]/div[1]/div[2]/div/div[2]/div[1]
-
+# /html/body/div[2]/div[1]/div[2]/div/div[2]/div[3] 
 # aたぐ
 # /html/body/div[2]/div[1]/div[2]/div/div[2]/div[2]/div[1]/div/div[2]/h3/a
 # /html/body/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div/h4/a
+
+# /html/body/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/div[1]/div/h4/a
