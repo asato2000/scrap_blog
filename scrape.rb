0a2795709fe79deb53107481ce152e0de42b67de
asato2000 <68488182+asato2000@users.noreply.github.com>
2021-10-06 15:04:21 +0900

    2

--- scrape.rb
+++ scrape.rb
@@ -20,17 +20,20 @@ end
 
 html = File.open('academy.html', 'r') {|f| f.read }
 doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
-section = doc.xpath('//*[@id="contents"]/div[1]/div[2]/div/div[2]/div[2]').first
 
-contents = {category: nil, topic: []}
-contents[:category] = section.xpath('./h2').first.text
-section.xpath('./div/div').each do |node|
+tech = []
+doc.xpath('//*[@id="contents"]/div[1]/div[2]/div/div[2]/div[2]').each do |section|
+
+ contents = {category: nil, topic: []}
+ contents[:category] = section.xpath('./h2').first.text
+ section.xpath('./div/div').each do |node|
   title = node.xpath('./div/h3/a').first.text
   url = node.xpath('./div/h3/a').first['href']
-topic = {title: title, url: url}
-contents[:topic] << topic
+ topic = {title: title, url: url}
+ contents[:topic] << topic
+ end
+ tech << contents
 end
-
-pp contents
+pp tech
 
 # write_file("academy.html",get_from("https://techacademy.jp/magazine/"))
