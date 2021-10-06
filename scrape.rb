2bf191dc668ea0a8a336dafc90d42091798a238c
asato2000 <68488182+asato2000@users.noreply.github.com>
2021-10-06 14:19:10 +0900

    スクレイピング成功

--- scrape.rb
+++ scrape.rb
@@ -21,11 +21,16 @@ end
 html = File.open('academy.html', 'r') {|f| f.read }
 doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
 section = doc.xpath('//*[@id="contents"]/div[1]/div[2]/div/div[2]/div[2]').first
-category = section.xpath("./h2").first.text
 
+contents = {category: nil, topic: []}
+contents[:category] = section.xpath('./h2').first.text
 section.xpath('./div/div').each do |node|
   title = node.xpath('./div/h3/a').first.text
   url = node.xpath('./div/h3/a').first['href']
+topic = {title: title, url: url}
+contents[:topic] << topic
 end
 
-write_file("academy.html",get_from("https://techacademy.jp/magazine/"))
+pp contents
+
+# write_file("academy.html",get_from("https://techacademy.jp/magazine/"))
